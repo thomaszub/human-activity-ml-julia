29251da1ad53cf0d485ecce7f65e4ce76d212020
thomaszub <thomas.zub@outlook.de>
2021-02-14 18:46:25 +0100

    Added simple LSTM model - currently not working

--- human-activity-lstm.jl
+++ human-activity-lstm.jl
@@ -20,6 +20,7 @@ feature_names = [
     "total_acc_z"
 ]
 
+using Flux: onehotbatch
 include("data-processing.jl")
 
 X_train = loadFeatureData(dir_train, feature_names, "train")
@@ -27,8 +28,37 @@ X_test = loadFeatureData(dir_test, feature_names, "test")
 
 y_train = loadLabelData("data/UCI HAR Dataset/train/y_train.txt")
 y_test = loadLabelData("data/UCI HAR Dataset/test/y_test.txt")
+yh_train = onehotbatch(y_train, 1:6)
+yh_test = onehotbatch(y_test, 1:6)
 
 # Prepare batch loader for train data
 using Flux.Data: DataLoader
 
-train_loader = DataLoader((X_train, y_train), batchsize=32, shuffle=true)
+train_loader = DataLoader((X_train, yh_train), batchsize=32, shuffle=true)
+
+# Build model
+
+using Flux
+using Flux: @epochs
+using Flux.Losses
+
+model = Chain(
+    LSTM(9, 9),
+    Dense(9, 6),
+    softmax
+)
+
+opt = ADAM()
+
+function loss(x, y)
+    println()
+    println(size(x))
+    println(size(y))
+    yh = model.(x)
+    println(size(yh))
+    sum(logitcrossentropy.(yh, y))
+end
+
+
+#@epochs 2
+Flux.train!(loss, params(model), train_loader, opt)
